8e0a1e6955080c7d84202ffd9195f8936981d8a1
Aditya Patil <aditya.patil87@gmail.com>
2014-06-03 18:26:55 -0700

    bug fix for oh wins

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -8,81 +8,187 @@ define(['angular'], function (angular) {
                             };
                         } ])
 
-                        .directive('gameDiv', ['$log','PubNub',function($log,PubNub){
+                        .directive('gameDiv', ['$log','PubNub','$rootScope','$routeParams','$timeout',function($log,PubNub,$rootScope,$routeParams,$timeout){
                             return {
                                 restrict: 'E',
                                 templateUrl: 'partials/gamediv.html',
                                 link: function(scope, elm, attrs) {
-                                    scope.uid = 'p'+Math.random();
+                                    function genRoomID(){
+                                        //return chance.word(10);
+                                        return 'abcdef';
+                                    }
+                                    var uid = chance.word(10);
+                                    scope.initialize_game = function(){
+                                        scope.play_boxes = {};
+                                        scope.room_id = $routeParams.room_id || genRoomID();
+                                        scope.turn = ($routeParams.room_id) ? false : true;
+                                        scope.theChannel = 'exesandohs_'+scope.room_id;
+                                        scope.character = ($routeParams.room_id) ? 'o' : 'x';
+                                        scope.play_boxes = initialize_play_boxes();
+                                        scope.winner = null;
+                                        scope.user = {
+                                            uid: uid,
+                                            character: scope.character,
+                                            boxes: []
+                                        };
+                                        scope.players_obj = {};
+                                        scope.players_obj[scope.user.uid] = scope.user;
+
+                                        // initialize_pubnub();
+
+                                        function initialize_play_boxes(){
+                                            var boxes = [];
+                                            for(var i=0;i<9;i++){
+                                                boxes[i] = {
+                                                    id: i,
+                                                    character: null,
+                                                    uid: null,
+                                                    updated: null
+                                                };
+                                            }
+                                            return boxes;
+                                        }
+                                    };
+
+                                    scope.initialize_game();
+
+                                    // function initialize_pubnub(){
                                     PubNub.init({
                                         publish_key: 'pub-cdc72730-41c8-4929-ab40-355c0b2cab4b',
                                         subscribe_key: 'sub-faf7eb11-0d87-11e2-8899-95dd86ce2293',
-                                        uuid: scope.uid
+                                        uuid: scope.user.uid
                                     });
-                                    scope.play_boxes = {};
-                                    scope.turn = true;
-                                    for(var i=0;i<9;i++){
-                                        scope.play_boxes[i] = {
-                                            id: i,
-                                            val: null,
-                                            player: null,
-                                            updated: null
-                                        };
+
+                                    PubNub.ngSubscribe({
+                                        channel: scope.theChannel
+                                    });
+
+                                    PubNub.ngHereNow({channel:scope.theChannel});
+                                    $rootScope.$on(PubNub.ngPrsEv(scope.theChannel), function(event, payload) {
+                                      scope.users = PubNub.ngListPresence(scope.theChannel);
+                                      console.log(scope.users);
+                                    });
+
+                                    startIdenting();
+
+                                    function startIdenting(){
+                                        identMessage();
                                     }
 
-                                    function selectBox(box){
-                                        if(scope.play_boxes[box].val!==null){
+                                    function identMessage(){
+                                        $timeout(function(){
+                                            PubNub.ngPublish({
+                                                channel: scope.theChannel,
+                                                message: { type: 'ident',
+                                                           payload: scope.players_obj
+                                                       }
+                                            });
+                                            identMessage();
+                                        },5000);
+                                    }
+
+                                    $rootScope.$on(PubNub.ngMsgEv(scope.theChannel), function(event, payload) {
+                                        // payload contains message, channel, env...
+                                        console.log('got a message event:', payload.message);
+                                        var msg = payload.message;
+                                        if(msg.type == 'turn'){
+                                            var box = selectBox(msg.payload.box.id,msg.payload.user);
+                                            if(msg.payload.user.uid!==scope.user.uid){
+                                                scope.turn = !scope.turn;
+                                            }
+                                            scope.finishTurn();
+                                        }else if(msg.type == 'ident'){
+                                            identOpponent(msg.payload);
+                                        }else if(msg.type == 'reset'){
+                                            scope.initialize_game();
+                                        }
+                                        // $.extend(scope.play_boxes[msg.id],msg);
+                                        $rootScope.$apply();
+                                    });
+                                    // }
+
+                                    function identOpponent(players_obj){
+                                        $.extend(scope.players_obj,players_obj);
+                                        scope.opponent = (function(obj){
+                                            for(var k in obj){
+                                                if(k != scope.user.uid){
+                                                    return obj[k];
+                                                }
+                                            }
+                                        })(players_obj);
+                                        // identSelf();
+                                    }
+
+                                    function selectBox(box_id,user){
+                                        if(scope.play_boxes[box_id].character!==null){
                                             $log.info('Box unavailable');
                                             return false;
                                         }
-                                        scope.play_boxes[box].val = (scope.turn) ? 'x' : 'o';
-                                        scope.play_boxes[box].player = 'adi';
-                                        scope.play_boxes[box].updated = parseInt(new Date()/1000,10);
-                                        return scope.play_boxes[box];
+                                        scope.play_boxes[box_id].character = user.character;
+                                        scope.play_boxes[box_id].uid = user.uid;
+                                        scope.play_boxes[box_id].updated = parseInt(new Date()/1000,10);
+                                        return scope.play_boxes[box_id];
                                     }
 
-                                    PubNub.ngSubscribe({
-                                        channel:'exesandohs',
-                                        message:function(res){
-                                            $.extend(scope.play_boxes[res[0].id],res[0]);
-                                            scope.$apply();
-                                        },
-                                        presence:function(res){
-                                            console.log('presence: ',res);
-                                        }
-                                    });
-
                                     scope.takeTurn = function(box_id){
-                                        var box = selectBox(box_id);
-                                        if(box){
-                                            var won = checkIfWon(scope.play_boxes);
-                                            if(won){
-                                                alert(won+' won');
-                                            }
-                                            PubNub.ngPublish({
-                                                channel: 'exesandohs',
-                                                message: box
-                                              });
-                                            scope.turn = !scope.turn;
+                                        if(scope.winner!==null){
+                                            return false;
                                         }
+                                        if(!scope.turn){
+                                            $log.error('Not your turn');
+                                            return false;
+                                        }
+                                        var box = selectBox(box_id,scope.user);
+                                        if(box){
+                                            PubNub.ngPublish({
+                                                channel: scope.theChannel,
+                                                message: {
+                                                    type: 'turn',
+                                                    payload: {
+                                                        user: scope.user,
+                                                        box: box
+                                                    }
+                                                }
+                                            });
+                                            scope.turn = !scope.turn;
+                                            scope.finishTurn();
+                                        }
+                                    };
 
+                                    scope.finishTurn = function(box){
+                                        var won = checkIfWon(scope.play_boxes);
+                                        if(won){
+                                            scope.winner = won;
+                                        }
+                                    };
+
+                                    scope.reset_game = function(){
+                                        PubNub.ngPublish({
+                                            channel: scope.theChannel,
+                                            message: {
+                                                type: 'reset',
+                                                payload: {
+                                                    user: scope.user
+                                                }
+                                            }
+                                        });
                                     };
 
                                     function checkIfWon(boxes){
                                         var values = new Array(9);
                                         var win_combos = [
-                                            [0,4,8],
-                                            [0,3,6],
                                             [0,1,2],
-                                            [2,5,8],
                                             [0,3,6],
-                                            [6,7,8],
+                                            [0,4,8],
+                                            [1,4,7],
                                             [2,4,6],
-                                            [1,4,7]
+                                            [2,5,8],
+                                            [3,4,5],
+                                            [6,7,8]
                                         ];
                                         for(var box_id in boxes){
-                                            if(boxes[box_id].val!==null){
-                                                values[box_id] = boxes[box_id].val;
+                                            if(boxes[box_id].character!==null){
+                                                values[box_id] = boxes[box_id].character;
                                             }
                                         }
                                         var counts = {
@@ -94,21 +200,27 @@ define(['angular'], function (angular) {
                                                 counts[values[i]].push(i);
                                             }
                                         }
-                                        for(var val in counts){
+                                        for(var character in counts){
                                             for(var i=0;i<win_combos.length;i++){
                                                 var diff = [];
+                                                console.log(counts[character],win_combos[i]);
                                                 $.each(win_combos[i], function(key,value) {
-                                                    if (-1 === counts[val].indexOf(value)) {
+                                                    if (-1 === counts[character].indexOf(value)) {
                                                         diff.push(value);
                                                     }
                                                 });
+                                                console.log(diff);
                                                 if(diff.length===0){
-                                                    $log.info('GAME OVER! '+val+' won!');
-                                                    return val;
+                                                    $log.info('GAME OVER! '+character+' won!');
+                                                    $log.info('Winning combo: ',win_combos[i]);
+                                                    for(var j=0;j<win_combos[j].length;j++){
+                                                        scope.play_boxes[win_combos[i][j]].won = true;
+                                                    }
+                                                    return character;
                                                 }
                                             }
-                                            return false;
                                         }
+                                        return false;
                                     }
                                 }
                             };
